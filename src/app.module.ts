1e1cbfcd5be9bfd63985ed71060e705b6823e601
Erik Vergara <evergarah2000@gmail.com>
2024-10-29 10:28:13 -0500

    base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-import { CarsModule } from './cars/cars.module';
-import { BrandsModule } from './brands/brands.module';
-import { SeedModule } from './seed/seed.module';
+
 
 @Module({
-  imports: [CarsModule, BrandsModule, SeedModule],
+  imports: [],
   controllers: [],
   providers: [],
   exports: [],
--- src/cars/dto/index.ts
+++
@@ -1,3 +0,0 @@
-/** archivo valde para la centralizacion de modulos */
-export { CreateCarDto } from "./create.car.dto";
-export { UpdateCarDto } from './update.car.dto';
