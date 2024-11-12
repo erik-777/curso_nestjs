dc6d3ef1032a7f13d9cd23f6df96b3e8a1daf4cd
Erik Vergara <evergara@idtolu.com>
2024-11-12 19:30:36 +0000

    se agrega el dto

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsNumber, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+    
+    @IsString()
+    @MinLength(1) 
+    name: string;
+
+    @IsPositive()
+    @IsNumber()
+    @Min(1)
+    no: number;
+}
