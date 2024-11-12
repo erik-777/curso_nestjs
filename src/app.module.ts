69c2a314e8fb79cf41deb28134608aa49a4580a1
Erik Vergara <evergara@idtolu.com>
2024-11-12 22:36:55 +0000

    agrego delete y custom pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 
 @Module({
   imports: [ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..', 'public')
   }), MongooseModule.forRoot('mongodb://pokedex_user:admin12345@10.0.102.9:27018/pokedex'),
-    PokemonModule],
+    PokemonModule,
+    CommonModule],
   controllers: [],
   providers: [],
   exports: [],
