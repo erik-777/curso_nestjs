6a1c393b68e2394bf480ca9b4e3ba6a25d7ba92e
Erik Vergara <evergara@idtolu.com>
2024-11-14 15:09:43 +0000

    get Pokemons

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 
 @Injectable()
 export class SeedService {
 
-  executeSeed() {
-    return 'Seed executed';
+  private readonly axios: AxiosInstance = axios;
+
+
+  async executeSeed() {
+
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    
+    data.results.map(async ({ name, url }) => {
+      console.log(name)
+      console.log(url)
+
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+      console.log(no)
+      //await this.pokemonModel.create({ no, name })
+    })
+
+    return data.results
   }
 }
