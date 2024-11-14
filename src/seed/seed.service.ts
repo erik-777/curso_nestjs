b99e983c0c3f3277187cd2c30680f9cf9ceb6346
Erik Vergara <evergara@idtolu.com>
2024-11-14 16:26:22 +0000

    add insert Sedd on mongo

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
+
 
 
 @Injectable()
 export class SeedService {
 
+
   private readonly axios: AxiosInstance = axios;
+  constructor(
+    private readonly pokemonService: PokemonService
+
+  ) { }
 
 
   async executeSeed() {
 
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
-    
-    data.results.map(async ({ name, url }) => {
-      console.log(name)
-      console.log(url)
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
 
+    data.results.map(async ({ name, url }) => {
+     
       const segments = url.split('/');
+
       const no: number = +segments[segments.length - 2];
-      console.log(no)
-      //await this.pokemonModel.create({ no, name })
+     
+      await this.pokemonService.create({ name, no });
+
     })
 
     return data.results
