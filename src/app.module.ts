87025ff01c043809888f0024f9cb68fdfb36f1e5
erik-777 <evergarah2000@gmail.com>
2024-11-26 19:04:42 +0000

    add dto product

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Inject, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
