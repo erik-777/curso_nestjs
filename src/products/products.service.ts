e70e3954d405be88538c7132def0bc6e63857999
erik-777 <evergarah2000@gmail.com>
2024-11-26 19:56:43 +0000

    add the create service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>,
+
+  ) {
+
+  }
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productsRepository.create(createProductDto);
+
+      await this.productsRepository.save(product);
+
+      return product;
+
+    } catch (error) {
+
+      this.handleException(error);
+    }
+
+
   }
 
-  findAll() {
-    return `This action returns all products`;
+
+  async findAll() {
+    try {
+      const products = await this.productsRepository.find();
+      return products;
+    } catch (error) {
+      throw new InternalServerErrorException('Error getting products');
+    }
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} product`;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+    return `This action updates a #${id} product `;
   }
 
   remove(id: number) {
     return `This action removes a #${id} product`;
   }
+  private handleException(error: any,) {
+
+
+
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+    this.logger.error(error);
+
+    throw new InternalServerErrorException('Unexpected error, check service');
+  }
+
 }
